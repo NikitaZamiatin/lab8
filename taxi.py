2340ada2fea66a2876e85ec5edd4cb8c97970b7a
NikitaZamiatin <nikitvzamatin16@gmail.com>
2023-11-12 09:45:33 +0500

    Изменения
    Добавил изменения для лучшего вывода

--- taxi.py
+++ taxi.py
@@ -2,7 +2,7 @@ N = int(input("Введите натуральное число N от 1 до 10
 
 distances = list(map(int, input("Введите N положительных целых чисел, задающих расстояния в километрах от работы до домов сотрудников компании: ").split()))
 
-tariffs = list(map(int, input("Далее еще N положительных целых чисел чисел — тарифы в рублях за проезд одного километра в такси").split()))
+tariffs = list(map(int, input("Далее еще N положительных целых чисел чисел — тарифы в рублях за проезд одного километра в такси: ").split()))
 
 employees = [(i+1, distances[i]) for i in range(N)]
 employees.sort(key=lambda x: x[1])
@@ -22,7 +22,14 @@ total_cost = 0
 for i in range(N):
     total_cost += employees[i][1] * taxis[result[i][1]-1][1]
 
-print(*result)
+result_fin = []
+
+for i in range(len(result)):
+    result_fin.append(result[i][0])
+
+print(*result_fin)
+
+print(total_cost)
 
 def spell_out_number(number):
     words = ['рубль', 'рубля', 'рублей']
